2f4764a810ec7d0ee4cb86253925e5690cc64902
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-09-21 16:59:41 +0100

    added data digitization

--- cls/cls_write_to_file.py
+++ cls/cls_write_to_file.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import os
+from locale import atof
 from pathlib import Path
 
 
@@ -18,20 +19,34 @@ class WriteToFile:
         start_date = datetime.datetime.today()
         print(f"date: {start_date}")
 
-        self.file_name = f'{self.full_path}\\measurements_{start_date.day}.{start_date.month}.{start_date.year}_{start_date.hour}{start_date.minute}{start_date.second}.txt '
+        self.file_name = f'{self.full_path}\\measurements_{start_date.day}.{start_date.month}.{start_date.year}_{start_date.hour}{start_date.minute}{start_date.second}.csv '
         print(f'file_name: {self.file_name}')
 
         with open(self.file_name, 'w') as f:
-            f.writelines(f"Time, s\t Voltage, V\n")
+            f.writelines(f"N,Time_s,Voltage_V\n")
 
         self.start_time = datetime.datetime.fromtimestamp(time.mktime(time.gmtime()))
+        self.point_count = 0
 
-    def record_data(self, data: str):
+    def record_data(self, data: str) -> str:
+
+        dig_data = 0
+
+        try:
+            dig_data = atof(data)
+        except ValueError:
+            print('Data Value Error')
+            return 'Error'
+
         point_time = datetime.datetime.today()
         d_time = datetime.timedelta.total_seconds(point_time - self.start_time)
-        data_str = f'{d_time}\t{data}'
+        self.point_count += 1
 
-        print(f'Record measurement point. time:\t{d_time}\tvalue:{data}')
+        data_str = f'{self.point_count},{d_time},{dig_data}'
+
+        print(f'Record measurement point. N:{self.point_count}\t time:\t{d_time}\tvalue:{dig_data}')
 
         with open(self.file_name, 'a') as f:
             f.write(f"{data_str}\n")
+
+        return 'Ok'
--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from cls.cls_write_to_file import WriteToFile
 if __name__ == '__main__':
     device_m = DeviceM('instr_idn.txt')
 
-    w_2_file = WriteToFile('data')
+    folder = input("Input here your folder name('d' to use default 'data'):\t")
 
-    for i in range(5):
+    if folder == 'd':
+        folder = 'data'
+    w_2_file = WriteToFile(folder)
+
+    for i in range(50):
         time.sleep(0.3)
         data = device_m.make_meas()
         w_2_file.record_data(data)
